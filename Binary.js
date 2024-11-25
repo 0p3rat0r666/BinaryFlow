38b01b64bed81543c0cd6f78e946d9fd55fdc280
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-25 14:18:20 -0600

    Create Binary.js

---
+++ Binary.js
@@ -0,0 +1,18 @@
+// move one line down as a test
+console.log("1");
+console.log("0");
+console.log("1");
+console.log("1");
+console.clear();
+console.log("");
+console.log("1");
+console.log("0");
+console.log("1");
+console.log("1");
+console.clear();
+console.log("");
+console.log("");
+console.log("1");
+console.log("0");
+console.log("1");
+console.log("1");
